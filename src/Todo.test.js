c4fdf71af0a1c3de11d112c11677c8c0cde8c560
Anshul Dahiya <anshul.rts@gmail.com>
2019-03-20 14:15:12 +0530

    Setting up Enzyme and its basics

--- src/Todo.test.js
+++ src/Todo.test.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
+import  { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
 import Todo from './Todo';
 
-it("renders wihtout crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Todo />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe(Todo, () => {
+    it("renders wihtout crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<Todo />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
