8537fdc58439f0ca573a8391f299d4dba181701b
Anshul Dahiya <anshul.rts@gmail.com>
2019-03-20 14:30:41 +0530

    Writing a snapshot test

--- src/Todo.test.js
+++ src/Todo.test.js
@@ -5,10 +5,22 @@ import renderer from 'react-test-renderer';
 
 import Todo from './Todo';
 
+const description = "New Todo";
+const mockRemoveTodo = jest.fn();
+const component = shallow(
+    <Todo description={description} removeTodo={mockRemoveTodo} />
+);
+
 describe(Todo, () => {
     it("renders wihtout crashing", () => {
         const div = document.createElement("div");
         ReactDOM.render(<Todo />, div);
         ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it("renders and matches our snapshot", () => {
+        const component = renderer.create(<Todo description="Yo" />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
     });
 });
