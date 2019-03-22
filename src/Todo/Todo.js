a041dc9ecc53d4664cc6a3a8e0f970a8846880d2
Anshul Dahiya <anshul.rts@gmail.com>
2019-03-22 15:23:14 +0530

    CSS composability - Adding few todo's as critical

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -8,10 +8,12 @@ class Todo extends Component {
         this.state = {
             description: this.props.description,
             done: false,
+            critical: false
         }
 
         this.markAsDone = this.markAsDone.bind(this);
         this.removeTodo = this.removeTodo.bind(this);
+        this.markCritical = this.markCritical.bind(this);
     }
 
     markAsDone() {
@@ -19,7 +21,14 @@ class Todo extends Component {
     }
 
     cssClasses() {
-        let classes = [styles.todo];
+        let classes = [];
+
+        if(this.state.critical) {
+            classes = [styles.critical];
+        }
+        else {
+            classes = [styles.todo];
+        }
         if(this.state.done) {
             classes = [...classes, styles.done];
         }
@@ -30,14 +39,19 @@ class Todo extends Component {
         this.props.removeTodo(this.state.description);
     }
 
+    markCritical() {
+        this.setState({ critical: true });
+    }
+
     render() {
         return (
             <div className={this.cssClasses()}>
                 { this.state.description }
             <br />
             <hr className={styles.redDivider} />
-            <button className="MarkDone" onClick={this.markAsDone}>Mark as Done</button>
-            <button className="RemoveTodo" onClick={this.removeTodo}>Remove Me</button>
+            <button className="MarkDone" onClick={ this.markAsDone }>Mark as Done</button>
+            <button className="RemoveTodo" onClick={ this.removeTodo }>Remove Me</button>
+            <button className="MarkCritical" onClick={ this.markCritical }>Mark as Critical</button>
             </div>
         );
     }
